6e3196b33bd040e1c9eac3d7a6b762e842c616fc
Nisha mahar <142042704+Nishamahar@users.noreply.github.com>
2023-10-12 19:57:02 +0530

    added script.js

---
+++ script.js
@@ -0,0 +1,28 @@
+// Get the display element and buttons
+const display = document.querySelector('.display');
+const buttons = document.querySelectorAll('button');
+
+// Add a click event listener to each button
+buttons.forEach(button => {
+  button.addEventListener('click', () => {
+    const buttonValue = button.getAttribute('data-value');
+
+    if (buttonValue === 'AC') {
+      // Clear the display
+      display.value = '';
+    } else if (buttonValue === 'DEL') {
+      // Delete the last character
+      display.value = display.value.slice(0, -1);
+    } else if (buttonValue === '=') {
+      // Evaluate the expression and display the result
+      try {
+        display.value = eval(display.value);
+      } catch (error) {
+        display.value = 'Error';
+      }
+    } else {
+      // Append the button value to the display
+      display.value += buttonValue;
+    }
+  });
+});
