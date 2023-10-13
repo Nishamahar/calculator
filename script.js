7cc97ff709a73950368d3c72b480a7d17c06f969
Nisha mahar <142042704+Nishamahar@users.noreply.github.com>
2023-10-13 21:02:55 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,64 +1,28 @@
+// Get the display element and buttons
 const display = document.querySelector('.display');
 const buttons = document.querySelectorAll('button');
 
-buttons.forEach((button) => {
+// Add a click event listener to each button
+buttons.forEach(button => {
   button.addEventListener('click', () => {
-    const value = button.getAttribute('data-value');
-    const type = button.getAttribute('data-type');
+    const buttonValue = button.getAttribute('data-value');
 
-    if (type === 'function') {
-      handleFunction(value);
+    if (buttonValue === 'AC') {
+      // Clear the display
+      display.value = '';
+    } else if (buttonValue === 'DEL') {
+      // Delete the last character
+      display.value = display.value.slice(0, -1);
+    } else if (buttonValue === '=') {
+      // Evaluate the expression and display the result
+      try {
+        display.value = eval(display.value);
+      } catch (error) {
+        display.value = '';
+      }
     } else {
-      handleInput(value);
+      // Append the button value to the display
+      display.value += buttonValue;
     }
   });
 });
-
-function handleFunction(func) {
-  // Handle the math functions here
-  if (func === 'sqrt') {
-    const currentValue = parseFloat(display.value);
-    if (!isNaN(currentValue)) {
-      display.value = Math.sqrt(currentValue);
-    }
-  } else if (func === '^2') {
-    const currentValue = parseFloat(display.value);
-    if (!isNaN(currentValue)) {
-      display.value = currentValue * currentValue;
-    }
-  } else if (func === '^') {
-    // Handle x^y function
-  } else if (func === 'sin') {
-    const currentValue = parseFloat(display.value);
-    if (!isNaN(currentValue)) {
-      display.value = Math.sin(currentValue);
-    }
-  } else if (func === 'cos') {
-    const currentValue = parseFloat(display.value);
-    if (!isNaN(currentValue)) {
-      display.value = Math.cos(currentValue);
-    }
-  } else if (func === 'tan') {
-    const currentValue = parseFloat(display.value);
-    if (!isNaN(currentValue)) {
-      display.value = Math.tan(currentValue);
-    }
-  } else if (func === 'ln') {
-    const currentValue = parseFloat(display.value);
-    if (!isNaN(currentValue) && currentValue > 0) {
-      display.value = Math.log(currentValue);
-    }
-  } else if (func === 'log') {
-    const currentValue = parseFloat(display.value);
-    if (!isNaN(currentValue) && currentValue > 0) {
-      display.value = Math.log10(currentValue);
-    }
-  }
-}
-
-function handleInput(value) {
-  // Handle numeric and operator input here
-  // You can modify your existing code for handling numbers and operators
-}
-
-// Add your calculator logic for handling numbers and operators here
