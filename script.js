3361dfc9b0c2881e849e6db3302be92661ac2af2
Nisha mahar <142042704+Nishamahar@users.noreply.github.com>
2023-10-13 21:10:36 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,28 +1,29 @@
-// Get the display element and buttons
-const display = document.querySelector('.display');
-const buttons = document.querySelectorAll('button');
+const display = document.querySelector(".display");
+const buttons = document.querySelectorAll("button");
+const specialChars = ["%", "*", "/", "-", "+", "="];
+let output = "";
 
-// Add a click event listener to each button
-buttons.forEach(button => {
-  button.addEventListener('click', () => {
-    const buttonValue = button.getAttribute('data-value');
+//Define function to calculate based on button clicked.
+const calculate = (btnValue) => {
+  display.focus();
+  if (btnValue === "=" && output !== "") {
+    //If output has '%', replace with '/100' before evaluating.
+    output = eval(output.replace("%", "/100"));
+  } else if (btnValue === "AC") {
+    output = "";
+  } else if (btnValue === "DEL") {
+    //If DEL button is clicked, remove the last character from the output.
+    output = output.toString().slice(0, -1);
+  } else {
+    //If output is empty and button is specialChars then return
+    if (output === "" && specialChars.includes(btnValue)) return;
+    output += btnValue;
+  }
+  display.value = output;
+};
 
-    if (buttonValue === 'AC') {
-      // Clear the display
-      display.value = '';
-    } else if (buttonValue === 'DEL') {
-      // Delete the last character
-      display.value = display.value.slice(0, -1);
-    } else if (buttonValue === '=') {
-      // Evaluate the expression and display the result
-      try {
-        display.value = eval(display.value);
-      } catch (error) {
-        display.value = '';
-      }
-    } else {
-      // Append the button value to the display
-      display.value += buttonValue;
-    }
-  });
+//Add event listener to buttons, call calculate() on click.
+buttons.forEach((button) => {
+  //Button click listener calls calculate() with dataset value as argument.
+  button.addEventListener("click", (e) => calculate(e.target.dataset.value));
 });
